402303b20b96df0589f4831df73f4735d67780ef
nicolevenachi <ludwing@unicauca.edu.co>
2024-02-24 18:17:25 -0500

    filters in getAllJobs -> pagination done

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -22,10 +22,10 @@ const getAllJobs = async (req, res) => {
 
   // *** chaining all of the remaining data filter properties ***
   if (status && status !== 'all') {
-    queryObject. status = status
+    queryObject.status = status
   }
   if (jobType && jobType !== 'all') {
-    queryObject. jobType = jobType
+    queryObject.jobType = jobType
   }
 
   
@@ -53,11 +53,24 @@ const getAllJobs = async (req, res) => {
     default:
       break;
   }
+  
+  // -> Pagination (limiting also the amount of data sended)
 
+  // setup page, limit and skig (to jump over some registers and go to the selectred page)
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  // additional data (to paginatio fronted setup)
+
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs/limit); // ceil -> simpere una pagina de mas, si se pasa el limit
   // *** response
   // default behavior -> Sino hay amtch, no retorna ningun Job, sino todos los Jobs (por mongoose 6, mongoose 5 es al reves)
   const jobs = await result
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages})
 }
 
 const getJob = async (req, res) => {
