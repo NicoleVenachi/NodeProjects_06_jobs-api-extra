8acd82ae875afa206797f0edad8303c011c161c1
nicolevenachi <ludwing@unicauca.edu.co>
2024-02-25 18:41:41 -0500

    permissions -> check/set user permissions in auth middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -12,8 +12,12 @@ const auth = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+    // --- Permisos --- Basicamente, setear una vraible para saber que permisos tendra
+    const testUser = payload.userId === '65c9496d9ab8510140e6cc8e'
+
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    req.user = { userId: payload.userId, testUser }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
