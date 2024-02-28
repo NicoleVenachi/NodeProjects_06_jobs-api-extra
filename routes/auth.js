914653e26e2e6a1697bfb360fca2f18e91c447e2
nicolevenachi <ludwing@unicauca.edu.co>
2024-02-28 18:53:23 -0500

    api requests limiter to certain routes

--- routes/auth.js
+++ routes/auth.js
@@ -6,14 +6,26 @@ const { login, register, updateUser } = require('../controllers/auth');
 const authenticateUser = require('../middleware/authentication');
 const testUser = require('../middleware/testUser');
 
+
+const rateLimiter = require('express-rate-limit')
+const apiLimiter =  rateLimiter(
+  {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message: {
+      msg: 'Too many requests from this IP. Please tryu agaom after 15 minutes'
+    }
+  }
+)
+
 // *** inicializo router ***
 const router = express.Router();
 
 // *** Routing ***
 
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', apiLimiter,  register)
+router.post('/login', apiLimiter, login)
 router.patch('/updateUser',authenticateUser, testUser, updateUser) // to update the user info, we need to authenticate the user, and extract the token data
 
 module.exports = router
